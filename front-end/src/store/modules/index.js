d8ae164f0616027a59881c429692cf2e25f18d9f
brunocarvalho7 <brunocarvalho287@gmail.com>
2022-10-19 12:17:41 -0300

    [SETUP][FD] - Set up Vuetify Dashboard Template and Static pages (#2)
    * [FD] - Setting Up Vuetify Dashboard Template
    
    * [FD] - Setting Up Static Pages

---
+++ front-end/src/store/modules/index.js
@@ -0,0 +1,4 @@
+export { default as app } from './app';
+export { default as authentication } from './authentication';
+export { default as sales } from './sales'
+export { default as user } from './user'
