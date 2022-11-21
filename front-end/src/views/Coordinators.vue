e2028a0c293e58328b80ba12ec25583c9f777b68
brunocarvalho7 <brunocarvalho287@gmail.com>
2022-11-21 14:02:50 -0300

    [FD] - Components Improvements (#16)
    * [FD] - Fixed the loading state for the Login button
    
    * [FD] - Placing the add coordinator fab button right above the One Signal fab button

--- front-end/src/views/Coordinators.vue
+++ front-end/src/views/Coordinators.vue
@@ -31,7 +31,7 @@
         <!-- Edit / New Coordinator Dialog -->
         <v-dialog v-model="dialog" max-width="500px">
             <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" class="mx-2" v-bind="attrs" v-on="on" absolute bottom right fab>
+                <v-btn color="primary" class="mb-14 mx-2" v-bind="attrs" v-on="on" absolute bottom right fab>
                     <v-icon dark>
                         mdi-plus
                     </v-icon>
